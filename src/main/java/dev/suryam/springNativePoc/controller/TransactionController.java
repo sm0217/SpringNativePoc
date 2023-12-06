dc1dcbf8583ded2331cb6bbd004a6f51f058b966
Surya Murali <surya.murali@icloud.com>
2023-12-06 13:59:48 +0000

    Hikarip connection settings

--- src/main/java/dev/suryam/springNativePoc/controller/TransactionController.java
+++ src/main/java/dev/suryam/springNativePoc/controller/TransactionController.java
@@ -3,17 +3,29 @@ package dev.suryam.springNativePoc.controller;
 import dev.suryam.springNativePoc.HttpCallService;
 import dev.suryam.springNativePoc.entity.Transaction;
 import dev.suryam.springNativePoc.repository.TransactionRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/transactions")
 public class TransactionController {
+
+    private static final Logger logger = LoggerFactory.getLogger(TransactionController.class);
 
     @Autowired
     private TransactionRepository repository;
@@ -21,20 +33,45 @@ public class TransactionController {
     @Autowired
     private HttpCallService httpCallService;
 
+    @Value("${config.config1}")
+    private String config1;
+
     @PostMapping()
     public ResponseEntity<Transaction> postTransaction(@RequestBody Transaction transaction) {
+        Random random = new Random();
+        Double randomNumber = random.nextDouble(100) + 1;
+        transaction.setPaymentAmount(randomNumber);
+        transaction.setCustomerId(generateRandom());
         Transaction savedTransaction = repository.save(transaction);
         return ResponseEntity.ok(savedTransaction);
     }
 
+    private String generateRandom() {
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 7;
+        Random random = new Random();
+
+        return random.ints(leftLimit, rightLimit + 1)
+                .limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+    }
+
     @GetMapping("/all")
     public ResponseEntity<List<Transaction>> getAllTransactions() {
-        List<Transaction> transactions = repository.findAll();
+        PageRequest pageRequest = PageRequest.of(0, 50, Sort.by("paymentAmount"));
+        List<Transaction> transactions = repository.findByPaymentAmountLessThan(50.0, pageRequest);
+        transactions.stream()
+                .map(Transaction::getCustomerId)
+                .map(String::toLowerCase)
+                .forEach(logger::info);
         return ResponseEntity.ok(transactions);
     }
 
     @GetMapping(value = "/states/{numberOfCalls}", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Map<String, Object>> getStates(@PathVariable("numberOfCalls") int numberOfCalls) throws Exception {
+        logger.error("Config based property: " + config1);
         List<Map<String, Object>> response = httpCallService.parallelHttpCalls(numberOfCalls);
         return response;
     }
--- src/main/java/dev/suryam/springNativePoc/repository/TransactionRepository.java
+++ src/main/java/dev/suryam/springNativePoc/repository/TransactionRepository.java
@@ -1,10 +1,15 @@
 package dev.suryam.springNativePoc.repository;
 
 import dev.suryam.springNativePoc.entity.Transaction;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    List<Transaction> findByPaymentAmountLessThan(double value, Pageable pageable);
 
 }
